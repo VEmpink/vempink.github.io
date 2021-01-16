3d3e15b895d3285d1a887d64752213a3b50529dd
VEmpink <fiermandt@gmail.com>
2021-01-16 10:45:10 +0700

    Update UI "Switch" component 💄

--- src/components/Banner/Switch.tsx
+++ src/components/Banner/Switch.tsx
@@ -1,7 +1,6 @@
 import {useContext} from 'react';
 import styled from 'styled-components/macro';
 import {CurrentThemeContext} from '../../context';
-import {Theme} from '../../types';
 
 const SwitchWrapper = styled.span`
   display: inline-block;
@@ -9,26 +8,30 @@ const SwitchWrapper = styled.span`
   margin-top: 40px;
 `;
 
-const SwitchTrack = styled.div`
+type SwitchTrackProps = {
+  currentTheme: 'light' | 'dark';
+};
+
+const SwitchTrack = styled.div<SwitchTrackProps>`
   width: 32px;
   height: 10px;
   border-radius: 10px;
-  /**
-   * Anoying in light mode
-   */
-  background-color: ${({theme}: {theme: Theme}) => theme.textHintColor};
+  background-color: ${(props) =>
+    props.currentTheme === 'light' ? '#D7E0E7' : '#526475'};
 `;
 
-const SwitchThumb = styled.div`
+type SwitchThumbProps = {
+  currentTheme: 'light' | 'dark';
+};
+
+const SwitchThumb = styled.div<SwitchThumbProps>`
   position: absolute;
   top: 50%;
   width: 16px;
   height: 16px;
   border-radius: 50%;
-  /**
-   * Anoying in light mode
-   */
-  background-color: ${({theme}: {theme: Theme}) => theme.textPrimaryColor};
+  background-color: ${(props) =>
+    props.currentTheme === 'light' ? '#7F92A4' : '#7F92A4'};
   transform: translateY(-50%);
   transition: left 0.3s cubic-bezier(0.4, 0.03, 0, 1);
   cursor: pointer;
@@ -39,9 +42,10 @@ function Switch() {
 
   return (
     <SwitchWrapper>
-      <SwitchTrack />
+      <SwitchTrack currentTheme={theme} />
 
       <SwitchThumb
+        currentTheme={theme}
         style={{left: theme === 'light' ? 0 : 16}}
         onClick={() => {
           setTheme(theme === 'light' ? 'dark' : 'light');
